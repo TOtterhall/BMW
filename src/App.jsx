b2a8b0eb611ee0ffd12cc1c570ff4013984992f0
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-05-16 11:17:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />;
+}
+
+function App() {
+  return (
+    <>
+      <Canvas
+        dpr={[1, 2]}
+        shadows
+        camera={{ fov: 45 }}
+        style={{ position: "absolute" }}
+      >
+        <color attach="background" args={["#101010"]} />
+        <PresentationControls
+          spedd={1.5}
+          global
+          zoom={0.5}
+          polar={[-0.1, Math.PI / 4]}
+        >
+          <Stage enviroment={null}>
+            <Model scale={0.01} />
+          </Stage>
+        </PresentationControls>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
